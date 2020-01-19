5da70b14af32635feeb3a4c6fa75dd088fc81f93
Vibhu Banka <vibhu@geekyants.com>
2020-01-20 02:27:27 +0530

    Added root saga

---
+++ src/sagas/index.js
@@ -0,0 +1,38 @@
+// actions -> each action -> handler -> generator function
+import { all, takeLatest, takeEvery, call, put } from 'redux-saga/effects'
+import types from '../actionTypes'
+
+const delay = ms => new Promise((resolve, reject) => setTimeout(resolve, ms))
+
+function* addTodo(action) {
+  // do something
+  console.log('$$$$ addTodo function', action)
+  yield
+}
+
+function* deleteTodo(action) {
+  yield
+}
+
+function* addTodoRequest(action) {
+  const { payload } = action // we are getting simple string of content
+
+  yield call(delay, 200)
+
+  yield put({
+    type: types.ADD_TODO_SUCCESS,
+    payload: {
+      caption: payload,
+      isCompleted: false
+    }
+  })
+}
+
+
+export default function* rootSaga() {
+  yield all([
+    takeLatest(types.ADD_TODO, addTodo),
+    takeLatest(types.DELETE_TODO, deleteTodo),
+    takeLatest(types.ADD_TODO_REQUEST, addTodoRequest)
+  ])
+}
