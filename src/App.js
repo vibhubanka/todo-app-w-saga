78c01cdbce54ff722d49d14f3b869a9096632a37
Vibhu Banka <vibhu@geekyants.com>
2020-01-20 02:30:39 +0530

    Updated app to render todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Todos test="some string----" />
   );
 }
+
+/*
+create a Todo
+delete
+
+read
+
+update
+
+
+store -> configure the store
+reducer -> update the store -> not do any computation/api call/manipulation etc.
+sagas -> communication through actions
+
+*/
 
 export default App;
