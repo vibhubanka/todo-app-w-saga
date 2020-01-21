0bb3241cf6a0d5f0b46c7b7cff26a183d4a45f20
Vibhu Banka <vibhu@geekyants.com>
2020-01-21 12:51:47 +0530

    Added comments, Implemented local state using hooks

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import types from '../actionTypes'
 import Todo from './Todo'
@@ -11,13 +11,26 @@ const todos = [
   { caption: 'dewdfwjchba', isCompleted: false }
 ]
 
+/*
+class components -> implemented through a class -> this.setState
+functional components -> implemented through function
+*/
+
+// class Todos extends Component -> lifecycle methods
+// onChange on an input -> this.setState({ inputValue: e.target.value })
+// updateValue(e.target.value)
 const Todos = props => {
-  console.log('$$$$ Todos props', props)
+  const [inputState, updateInputState] = useState('initialValue')
+  console.log('$$$$ value', inputState)
+  const fn = function () {
+    props.addTodo(inputState)
+  }
+
   return (
     <div>
       <div>
-        <input type="text"></input>
-        <button onClick={props.addTodo}>Add</button>
+        <input type="text" onChange={e => updateInputState(e.target.value)} value={inputState}></input>
+        <button onClick={() => props.addTodo(inputState)}>Add</button>
       </div>
 
       <div>
@@ -34,6 +47,20 @@ const Todos = props => {
     </div>
   )
 }
+/* Why the app is going into infinit loop when
+button's onClick was set to props.addTodo(inputState) instead of () => props.addTodo(inputState)
+
+-> first render
+-> addTodo called
+-> dispatched an action
+-> saga listened to it & reducer also updated the store with `loading: true`
+  -> store asked the component to update
+  -> addTodo called -> but it was not listened in saga because of `takeLatest`
+-> saga dispatched another action
+-> reducer updated the store with `todo: [...]`
+  -> store asked the component to update
+  -> addTodo called -> this time the saga listened
+*/
 
 const mapStateToProps = state => ({
   // should return an object, which is added to the component's props by `connect` HOC
@@ -43,10 +70,10 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   // should return an object, which is added to the component's props by `connect` HOC
-  addTodo: () => dispatch({
+  addTodo: (val) => dispatch({
     type: types.ADD_TODO_REQUEST,
-    payload: '11111'
-  })
+    payload: val
+  }) // returns undefined
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Todos)
